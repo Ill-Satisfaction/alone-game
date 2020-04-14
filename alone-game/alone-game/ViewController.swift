2d47c2f072f8a2518e8f3615daa7f6fe7bcff661
akiah tullis <akiahtullis@u.boisestate.edu>
2020-04-14 18:21:29 -0400

    initial commit

---
+++ alone-game/alone-game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alone-game
+//
+//  Created by XCodeClub on 2020-04-14.
+//  Copyright © 2020 250_dream_team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
