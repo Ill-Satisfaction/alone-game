77791a933e897c460a943d8693a970b915e1533e
akiah tullis <akiahtullis@u.boisestate.edu>
2020-04-21 20:45:09 -0400

    saving progress. DONT TOUCH - strange authentification errors

--- Alone-Comic/Alone-Comic/Alone-Comic/ViewController.swift
+++ Alone-Comic/Alone-Comic/Alone-Comic/ViewController.swift
@@ -11,35 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var backgroundImageView: UIImageView!
+    let screenSize: CGRect = UIScreen.main.bounds
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-       // backgroundImageView.image = imageResize(imageObj:  UIImage(named: "Kitchen extended" )!, sizeChange: <#T##CGSize#>())
-        VStack {
-            GeometryReader { geo in
-                Image("Example")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geo.size.width)
-            }
-        }
         self.view.backgroundColor = UIColor.black
+        //backgroundImageView.
 
     }
     
-    func imageResize(imageObj:UIImage, sizeChange:CGSize)-> UIImage {
-
-        let hasAlpha = false
-        let origin: CGPoint = CGPoint(x: 0, y: 0)
-        let scale: CGFloat = 0.0 // Automatically use scale factor of main screen
-
-        UIGraphicsBeginImageContextWithOptions(sizeChange, !hasAlpha, scale)
-        imageObj.draw(in: CGRect(origin: origin, size: sizeChange))
-
-        guard let scaledImage = UIGraphicsGetImageFromCurrentImageContext() else { return <#default value#> }
-        UIGraphicsEndImageContext() // !!!
-        return scaledImage
-    }
     
     
 
