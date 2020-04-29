e15e38315cf647d0b94afa246055011c10091cf8
Adam Azevedo <atd8128@gmail.com>
2020-04-28 22:38:52 -0600

    completed up to day 5, static panels

---
+++ Alone-Comic/Alone-Comic/Alone-Comic/d3_p2_2_UIView.swift
@@ -0,0 +1,82 @@
+//
+//  d3_p2_2_UIView.swift
+//  Alone-Comic
+//
+//  Created by Adam Azevedo on 4/28/20.
+//  Copyright © 2020 250_DreamTeam. All rights reserved.
+//
+
+import UIKit
+
+class d3_p2_2_UIView: UINavigationController {
+    
+    // set all subpanels IN ORDER
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [
+            UIStoryboard(name: "Main", bundle: nil) .instantiateViewController(withIdentifier: "d3_p2_2_a")
+        ]
+    }()
+    
+    private var currPanel : Int = 0
+    
+    
+    
+    override func viewDidLoad() {
+        //  initialize screen
+        super.viewDidLoad()
+        self.setNavigationBarHidden(true, animated: false)
+        
+        
+        //  load first panel
+        self.goForward()
+        
+        
+        
+        
+        
+        
+        
+        //  --------------------------------
+        //  THIS SECTION IS CHANGED FROM PANEL TO PANEL,
+        //  DEPENDING ON WHAT WE NEED
+        //  --------------------------------
+        
+        
+        //  --------------------------------
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    @objc func goForward() {
+        if currPanel==orderedViewControllers.count {
+            print ("hit the wall")
+            return
+        }
+        
+        self.pushViewController(orderedViewControllers[currPanel], animated: true)
+        currPanel+=1
+    }
+    
+    func goBack() {
+        if currPanel==0 {
+            return
+        }
+        
+        self.popViewController( animated: true )
+        currPanel-=1
+    }
+    
+}
+
+
+
+
+
+
+
